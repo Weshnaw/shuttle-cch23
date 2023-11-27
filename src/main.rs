090aceccdf3c223dc79794dd80a5df3ba4db7387
Brendon Fallquist <bfallquist@gmail.com>
2023-11-27 13:08:12 -0500

    cch challenge -1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn error() -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "cch challenge -1")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(error));
 
     Ok(router.into())
 }
